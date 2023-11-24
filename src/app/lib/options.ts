632fec9f144971290a193b0ddc63c59dcaa33934
lenkly <lenk.lydia@territory.group>
2023-11-24 11:54:32 +0100

    :sparkles: add zonk

---
+++ src/app/lib/options.ts
@@ -0,0 +1,28 @@
+import type { Options } from "@/components/Dropdown";
+
+export const options: Options[] = [
+  {
+    label: "Henning",
+    value: "henning",
+  },
+  {
+    label: "Tatti",
+    value: "tatti",
+  },
+  {
+    label: "Gamze",
+    value: "gamze",
+  },
+  {
+    label: "Flo",
+    value: "flo",
+  },
+  {
+    label: "Kerem",
+    value: "kerem",
+  },
+  {
+    label: "Arthur",
+    value: "arthur",
+  },
+];
